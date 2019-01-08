1a0776855746f8f9786d105b53c15a9d63829553
Rawr <All-Cashmere@users.noreply.github.com>
2019-01-08 17:42:37 -0500

    Create ex1.js

---
+++ ex1.js
@@ -0,0 +1,12 @@
+function toUpper(str) {
+    return str
+        .toLowerCase()
+        .split(' ')
+        .map(function(word) {
+            return word[0].toUpperCase() + word.substr(1);
+        })
+        .join(' ');
+     }
+     console.log(toUpper("Hello darkness my old friend"));
+
+
